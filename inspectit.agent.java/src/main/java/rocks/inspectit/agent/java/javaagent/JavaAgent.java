56d8ba0b2559faba8b5f3e90398743b76e487630
agd <agd@nbookagd.novatec-gmbh.de>
2017-11-11 17:14:49 +0100

    Moved drumpthread to a separate class in javaagent package, changed system.out.println to Logger.info

--- inspectit.agent.java/src/main/java/rocks/inspectit/agent/java/javaagent/JavaAgent.java
+++ inspectit.agent.java/src/main/java/rocks/inspectit/agent/java/javaagent/JavaAgent.java
@@ -12,8 +12,6 @@ import java.lang.instrument.IllegalClassFormatException;
 import java.lang.instrument.Instrumentation;
 import java.lang.instrument.UnmodifiableClassException;
 import java.lang.management.ManagementFactory;
-import java.lang.management.ThreadInfo;
-import java.lang.management.ThreadMXBean;
 import java.lang.reflect.Constructor;
 import java.net.URISyntaxException;
 import java.net.URL;
@@ -23,11 +21,8 @@ import java.security.CodeSource;
 import java.security.PermissionCollection;
 import java.security.ProtectionDomain;
 import java.util.Enumeration;
-import java.util.HashMap;
 import java.util.HashSet;
-import java.util.Iterator;
 import java.util.List;
-import java.util.Map;
 import java.util.Set;
 import java.util.Timer;
 import java.util.TimerTask;
@@ -92,44 +87,6 @@ public class JavaAgent implements ClassFileTransformer {
 	 *            instrumentation.
 	 */
 
-	/** This is a method for MBean ***************** MY EDIT ****************** **/
-	public static void DumpThreadsMBean() {   //Q:
-		final StringBuilder threadString = new StringBuilder();
-		final ThreadMXBean threadMXBean = ManagementFactory.getThreadMXBean();
-		final ThreadInfo[] infos = threadMXBean.getThreadInfo(threadMXBean.getAllThreadIds(), Integer.MAX_VALUE);
-
-		// to be able to exclude daemon thread, getting all threads and ids
-		Map threads = Thread.getAllStackTraces();
-		Map<Long, Thread> threadIds = new HashMap();
-		for (Iterator itr = threads.keySet().iterator(); itr.hasNext();) {
-			Thread th = (Thread) itr.next();
-			threadIds.put(th.getId(), th);
-		}
-
-		for (ThreadInfo info : infos) {
-			// excluding all daemon threads
-			if (threadIds.get(info.getThreadId()).isDaemon() == false) {
-				// thread name
-				threadString.append("Thread \"");
-				threadString.append(info.getThreadName());
-				threadString.append("\" ");
-				// thread state
-				final Thread.State _state = info.getThreadState();
-				threadString.append(_state);
-				// stack trace for it
-				final StackTraceElement[] stackTraceElements = info.getStackTrace();
-				for (final StackTraceElement stackTraceElement : stackTraceElements) {
-					threadString.append("\n    at ");
-					threadString.append(stackTraceElement);
-				}
-				threadString.append("\n\n");
-			}
-			System.out.println(threadString.toString());
-		}
-	}
-
-	/** This is a method for MBean ***************** END OF MY EDIT ****************** **/
-
 	public static void premain(String agentArgs, Instrumentation inst) {
 		instrumentation = inst;
 
@@ -181,12 +138,11 @@ public class JavaAgent implements ClassFileTransformer {
 			e.printStackTrace(); // NOPMD
 		}
 		// my edit *********************************
-		// Q: why did it work with timer and not with creating a new thread inside the premain
-		System.out.println("**********************Called premain**********************");
+		LOGGER.info("**********************Called premain**********************");
 		TimerTask task = new TimerTask() {
 			@Override
 			public void run() {
-				DumpThreadsMBean();
+				DumpThreadsMBean.DumpThreads();
 			}
 		};
 		Timer timer = new Timer();
